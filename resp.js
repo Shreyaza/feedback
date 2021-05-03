9c003ae75d348040c8e3a78500dd2041617f586e
Shreyaza <shreya.zalavadiya@yahoo.com>
2021-05-04 01:59:05 +0530

    commit

---
+++ resp.js
@@ -0,0 +1,18 @@
+part2 = document.querySelector('.part2')
+part2 = document.querySelector('.part2')
+review =document.querySelector('review')
+
+
+
+
+
+review.addEventlistner('click', ()=>{
+    alert('Review submitted')
+
+
+
+})
+
+
+
+
